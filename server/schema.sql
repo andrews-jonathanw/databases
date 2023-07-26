7f64eb1224cf4e4f082cb035bef99c420737f5d3
Alisa Bondar <alisa.k.bondar@gmail.com>
2023-07-26 17:30:57 -0400

    created tables in schema.sql

--- server/schema.sql
+++ server/schema.sql
@@ -2,15 +2,29 @@ CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
+/* Create other tables and define schemas for them here! */
+CREATE TABLE users (
   /* Describe your table here.*/
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  username CHAR(20)
 );
 
-/* Create other tables and define schemas for them here! */
+CREATE TABLE rooms (
+  /* Describe your table here.*/
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  roomname CHAR(20)
+);
 
-
-
-
+CREATE TABLE messages (
+  /* Describe your table here.*/
+  id INT AUTO_INCREMENT,
+  message CHAR(255),
+  username_id INT,
+  roomname_id INT,
+  PRIMARY KEY (id),
+  FOREIGN KEY (username_id) REFERENCES users(id),
+  FOREIGN KEY (roomname_id) REFERENCES rooms(id)
+);
 /*  Execute this file from the command line by typing:
  *    mysql -u root < server/schema.sql
  *  to create the database and the tables.*/
